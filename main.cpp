1ca4cb221b5ee342f6cb4cbcb2f2a2831e89675f
ryuyah2000 <ryuyah2000@users.noreply.github.com>
2024-09-23 11:16:52 -0700

    add another method to print stars (commented)

--- main.cpp
+++ main.cpp
@@ -9,9 +9,18 @@ int main() {
     std::cout << "How many rows? ";
     std::cin >> rows;
     
+    // creating a string for each row method
     for (int i = 0; i < rows; ++i) {
         std::cout << std::string(i, '*') << std::endl;
     }
+    
+    // nested for loop method
+    /*for (int i = 1; i <= rows; ++i) {
+        for (int j = 0; j < i; ++j) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }*/
 
     return 0;
 }
